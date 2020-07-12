c3e107418bb1c5c3417e5717fc8c829e6bae05a9
Teguh Sulistian <teguhsulistian17@gmail.com>
2020-07-12 14:42:59 +0700

    commit on master

--- cross_sounding.py
+++ cross_sounding.py
@@ -7,8 +7,6 @@ file = 'MALUKU_PERUM.csv'
 data = pd.read_csv(file, sep=',', header=0)
 data["datetime"] = data["date"] +" "+ data["time"]
 data = data.drop(["date", "time"], axis=1)
-print(data)
-print(data)
+data["datetime"] = pd.to_datetime(data["datetime"], dayfirst=True)
 
-###DATA
-###CEK
+print(data)
