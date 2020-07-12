492d9ec62aa93be6fd1380b098a7546bab62e238
Teguh Sulistian <teguhsulistian17@gmail.com>
2020-07-12 13:39:38 +0700

    commit

--- cross_sounding.py
+++ cross_sounding.py
@@ -8,3 +8,7 @@ data = pd.read_csv(file, sep=',', header=0)
 data["datetime"] = data["date"] +" "+ data["time"]
 data = data.drop(["date", "time"], axis=1)
 print(data)
+print(data)
+
+###DATA
+###CEK
