9d6d1d5dcef1140777deeb2c78cd209bf3239b89
Teguh Sulistian <67400700+teguhsulistian@users.noreply.github.com>
2020-07-12 12:54:37 +0700

    Add files via upload

---
+++ cross_sounding.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+file = 'MALUKU_PERUM.csv'
+
+data = pd.read_csv(file, sep=',', header=0)
+data["datetime"] = data["date"] +" "+ data["time"]
+data = data.drop(["date", "time"], axis=1)
+print(data)
