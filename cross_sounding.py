1dd5d0b16e8610c044b6052f1bc612f646b8cc9f
Teguh Sulistian <teguhsulistian17@gmail.com>
2020-07-12 15:35:04 +0700

    commit on master

--- cross_sounding.py
+++ cross_sounding.py
@@ -4,9 +4,13 @@ import pandas as pd
 
 file = 'MALUKU_PERUM.csv'
 
+#Import data
 data = pd.read_csv(file, sep=',', header=0)
 data["datetime"] = data["date"] +" "+ data["time"]
 data = data.drop(["date", "time"], axis=1)
 data["datetime"] = pd.to_datetime(data["datetime"], dayfirst=True)
 
-print(data)
+#Find Closest Point
+data_x= data["x"]
+data_x= data["y"]
+
